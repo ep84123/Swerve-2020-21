79e101c82f983704d688c694d782d125198f5117
itaigil492 <itaigil492@gmail.com>
2020-09-30 12:38:08 +0300

    Changed SwerveModule constructor + setAngle. Changed a few things in Chassis to make it prettier

--- src/main/java/edu/greenblitz/bigRodika/subsystems/SwerveModule.java
+++ src/main/java/edu/greenblitz/bigRodika/subsystems/SwerveModule.java
@@ -10,33 +10,28 @@ import edu.greenblitz.gblib.gears.GearDependentValue;
 
 public class SwerveModule extends GBSubsystem {
 
-    private static SwerveModule instance;
-    private WPI_TalonSRX mRotate;
-    private CANSparkMax mDrive;
-    private IEncoder angleEncoder;
+    private final WPI_TalonSRX mRotate;
+    private final CANSparkMax mDrive;
+    private final IEncoder angleEncoder;
+    private final int ID;
 
-    SwerveModule(int rotatePort, int drivePort) { // I'm not sure how to give port numbers in init' should i just add theme to init?
+    SwerveModule(int rotatePort, int drivePort, int ID) { // I'm not sure how to give port numbers in init' should i just add theme to init?
+        this.ID = ID;
         mRotate = new WPI_TalonSRX(rotatePort);
         mDrive = new CANSparkMax(drivePort, CANSparkMaxLowLevel.MotorType.kBrushless); // TODO: check device type (2nd arg)
         angleEncoder = new TalonEncoder(RobotMap.Limbo2.Chassis.SwerveModule.NORMALIZER,mRotate);// again, values from past code
     }
 
-    public static SwerveModule getInstance(){ return instance; }
-
-    public void setAngle(double dest){
-        double dAngle =  dest - getNormAngleDegs();
-        if(dAngle > 0 && dAngle < 180){
+    public void setAngle(double destAngleDegs){
+        double dAngle =  destAngleDegs - getNormAngleDegs();
+        if (dAngle == 0){
+            mRotate.set(0);
+        }
+        else if(dAngle > 0 && dAngle <= 180 || dAngle <= -180){
             mRotate.set(1);
         }
-        else if(dAngle > 0 && dAngle < 180){
+        else {
             mRotate.set(-1);
-        }
-
-        if(dAngle < 0 && dAngle < 180){
-            mRotate.set(-1);
-        }
-        else if(dAngle < 0 && dAngle < 180){
-            mRotate.set(1);
         }
     }
 
@@ -45,7 +40,7 @@ public class SwerveModule extends GBSubsystem {
     }
 
     public double getNormAngleRads() {
-        //I chose the numbers according to the values used in past code
+        // I chose the numbers according to the values used in past code
         return 2 * Math.PI * (angleEncoder.getNormalizedTicks() - 8974.0 / new GearDependentValue<>(28672.0,
                 28672.0).getValue());
     }
@@ -65,4 +60,6 @@ public class SwerveModule extends GBSubsystem {
     public IEncoder getAngleEncoder() {
         return angleEncoder;
     }
+
+    public int getID() { return ID; }
 }
