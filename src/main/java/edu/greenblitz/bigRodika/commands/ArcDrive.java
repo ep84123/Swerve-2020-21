3d118628c2c831d1eb0a93f53c904efaf21a3742
orel <orel.avr95@gmail.com>
2020-10-12 15:11:46 +0300

    Now I really Added ArcDrive ~orLAvr

--- src/main/java/edu/greenblitz/bigRodika/commands/ArcDrive.java
+++ src/main/java/edu/greenblitz/bigRodika/commands/ArcDrive.java
@@ -5,13 +5,42 @@ import edu.greenblitz.bigRodika.RobotMap;
 public class ArcDrive {
 
     private double robotLinVel, robotAngVel, robotAngle;
-    private double R, maxVel, maxRad;
+    private double mainR, maxVel, maxR;
+    private double W = RobotMap.Limbo2.Measures.WIDTH,
+            L = RobotMap.Limbo2.Measures.LENGTH,
+            d = RobotMap.Limbo2.Measures.WHEEL_DIST_FROM_CENTER;
 
     public ArcDrive(double robotLinVel, double robotAngVel, double robotAngle) {
         this.robotLinVel = robotLinVel;
         this.robotAngVel = robotAngVel;
         this.robotAngle = robotAngle;
-        this.R = this.robotLinVel/this.robotAngVel;
-        this.maxRad = this.R + RobotMap.Limbo2.
+        this.mainR = this.robotLinVel/this.robotAngVel;
+        this.maxR = this.mainR + d;
     }
+
+    private double calcVel(double angle){
+         double R = Math.sqrt(mainR*mainR + d*d - 2*Math.cos(angle));
+         return maxVel * R/maxR;
+    }
+
+
+    public double[] calcVelocities(){
+        double lengthAngle = Math.acos(1-(L*L / 2*d*d));
+        double widthAngle = 180 - lengthAngle;
+
+        double tlAngle = lengthAngle/2 + robotAngle; // assuming the wheels turns clockwise
+        double trAngle = tlAngle + widthAngle,
+                blAngle = tlAngle - lengthAngle,
+                brAngle = tlAngle- widthAngle - lengthAngle;
+
+        // top-left, top-right, bottom-left, bottom-right
+        double tl = calcVel(tlAngle),
+                tr = calcVel(trAngle),
+                bl = calcVel(blAngle),
+                br = calcVel(brAngle);
+
+        return new double[] {tl, tr, bl, br};
+
+    }
+
 }
