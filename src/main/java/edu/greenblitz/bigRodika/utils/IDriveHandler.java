d01ffea62f6b000670d854e46252fc5f6784db44
orel <orel.avr95@gmail.com>
2020-10-31 11:44:47 +0200

    forgot to add the changes to the last commit oops ~orLAvr

--- src/main/java/edu/greenblitz/bigRodika/utils/IDriveHandler.java
+++ src/main/java/edu/greenblitz/bigRodika/utils/IDriveHandler.java
@@ -1,2 +1,5 @@
-package edu.greenblitz.bigRodika.utils;public interface IDriveHandler {
+package edu.greenblitz.bigRodika.utils;
+
+public interface IDriveHandler {
+    boolean shouldGoBackwards(double linVel, double angle, double driverInput);
 }
--- src/main/java/edu/greenblitz/bigRodika/utils/StupidDH.java
+++ src/main/java/edu/greenblitz/bigRodika/utils/StupidDH.java
@@ -1,2 +1,16 @@
-package edu.greenblitz.bigRodika.utils;public class StupidDH {
+package edu.greenblitz.bigRodika.utils;
+
+public class StupidDH implements IDriveHandler {
+    /**
+     *
+     * @param linVel - Linear Velocity of the robot
+     * @param angle - the angle the driver wants to go to (assuming it's robot oriented)
+     * @param driverInput - the input of the driver, here will be treated as a boolean
+     * @return - if the angle is grater than 180 and driver asked our recommendation return true
+     */
+    @Override
+    public boolean shouldGoBackwards(double linVel, double angle, double driverInput) {
+        return driverInput == 1 && angle > 180;
+    }
+
 }
