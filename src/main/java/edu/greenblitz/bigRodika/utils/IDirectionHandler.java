018a8f513f20acaa737b21074da4cedbad4b52e3
orel <orel.avr95@gmail.com>
2020-11-01 15:52:50 +0200

    Added SimpleDH - 4 explenation about the function go to notion or ask me ~orLAvr

--- src/main/java/edu/greenblitz/bigRodika/utils/IDirectionHandler.java
+++ src/main/java/edu/greenblitz/bigRodika/utils/IDirectionHandler.java
@@ -1,5 +1,12 @@
 package edu.greenblitz.bigRodika.utils;
 
 public interface IDirectionHandler {
+    /**\
+     *
+     * @param angle - the angle of the robot in degrees as long as not mentioned otherwise
+     * @param linVel - the linear velocity of the robobt
+     * @param driverInput - how much the driver wanted to be corrected by the function (a number between 0 to 1 if not mentioned otherwise)
+     * @return  - if the robot should go inverted
+     */
     boolean shouldGoInverted(double angle, double linVel, double driverInput);
 }
--- src/main/java/edu/greenblitz/bigRodika/utils/SimpleDH.java
+++ src/main/java/edu/greenblitz/bigRodika/utils/SimpleDH.java
@@ -1,2 +1,18 @@
-package edu.greenblitz.bigRodika.utils;public class SimpleDH {
+package edu.greenblitz.bigRodika.utils;
+
+public class SimpleDH implements IDirectionHandler{
+
+    private final double DRIVER_INPUT_SCALE = 320;
+
+    @Override
+    public boolean shouldGoInverted(double angle, double linVel, double driverInput) {
+        driverInput = driverInput*DRIVER_INPUT_SCALE; //rescaling driver input to the scale we want
+        double val = ftv(angle, linVel, driverInput);
+        return val < 0; // if the value is negative we want to drive inverted
+    }
+
+    public static double ftv(double t, double v, double a){
+        return Math.signum(180 - 2*t) * (Math.pow(Math.abs(t-90), 1.5 )) + a*v;
+    }
+
 }
