9ee75586c7c0368fc5a98259a8317d41f8111b08
orel <orel.avr95@gmail.com>
2020-11-10 20:05:42 +0200

    Added the tests for SimpleDH ~orLAvr

--- src/main/java/edu/greenblitz/bigRodika/tests/SimpleDHTest.java
+++ src/main/java/edu/greenblitz/bigRodika/tests/SimpleDHTest.java
@@ -7,8 +7,31 @@ public class SimpleDHTest {
     void test(){
         SimpleDriverFriendlyDH ddh = new SimpleDriverFriendlyDH();
 
+        //Vel 0
         assert ddh.handle(10,0, new double[]{100,1.5});
         assert ddh.handle(90,0, new double[]{100,1.5});
         assert !ddh.handle(100,0, new double[]{100,1.5});
+
+        //Ang 170
+        assert !ddh.handle(170,0, new double[]{100, 1.5});
+        assert !ddh.handle(170,3, new double[]{100, 1.5});
+        assert !ddh.handle(170,2, new double[]{100, 1.5});
+
+        //Ang 0
+        assert !ddh.handle(0,0, new double[]{100, 1.5});
+        assert !ddh.handle(0,3, new double[]{100, 1.5});
+        assert !ddh.handle(0,2, new double[]{100, 1.5});
+
+        //Ang 0.1
+        assert !ddh.handle(0.1,0, new double[]{100, 1.5});
+        assert !ddh.handle(0.1,3, new double[]{100, 1.5});
+        assert !ddh.handle(0.1,2, new double[]{100, 1.5});
+
+        //Ang 90
+        assert !ddh.handle(0.1,1, new double[]{100, 1.5});
+        assert !ddh.handle(0.1,2, new double[]{100, 1.5});
+        assert !ddh.handle(0.1,3    , new double[]{100, 1.5});
+
+
     }
 }
